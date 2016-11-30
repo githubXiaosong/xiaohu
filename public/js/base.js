5b2d28b37b7bff419df21ce8c5959ccd769aa62c
XiaoSong <1098030258@qq.com>
2016-11-30 22:43:09 +0800

    ui-router 整体CSS
    搞了下ui-router把页面都可以访问了 整体的CSS代码写了一下

--- public/js/base.js
+++ public/js/base.js
@@ -1,1 +1,28 @@
-console.log(1);
+angular.module('xiaohu',['ui.router'])
+
+    .config(function($interpolateProvider
+        ,$stateProvider
+        ,$urlRouterProvider)
+    {
+        $interpolateProvider.startSymbol('[:');
+        $interpolateProvider.endSymbol(':]');
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            .state('home',{
+                url:'/home',//指定在这个页面之后的路由和对应的在ui-view中的模板
+                templateUrl:'home.tpl'
+            })
+
+            .state('login',{
+                url:'/login',
+                templateUrl:'login.tpl' //先在页面的script中寻找这个页面 若没有则像服务器端申请页面
+            })
+
+            .state('signup',{
+                url:'/signup',
+                templateUrl:'signup.tpl' //先在页面的script中寻找这个页面 若没有则像服务器端申请页面
+            })
+    })
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="zh">
+<html lang="zh" ng-app="xiaohu">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
@@ -13,6 +13,90 @@
 
 </head>
 <body>
-    XiaoSong
+    <div class="navbar clearfix">
+        <div class="fl navbar-item">
+            <div>
+            <a class="navbar-item brand" href="#home">晓乎</a>
+            <input class="" type="text">
+            </div>
+        </div>
+
+        <div class="fr">
+            <a class="navbar-item" href="#home">首页</a>
+            <a class="navbar-item" href="#login">登录</a>
+            <a class="navbar-item" href="#signup">注册</a>
+        </div>
+    </div>
+    {{-- ui-view 指令对应template中的内容 --}}
+    <div class="page">
+        <div ui-view/>
+    </div>
 </body>
+
+
+
+{{--.page .home--}}
+<script type="text/ng-template" id="home.tpl" >
+    <div class="home container">
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+        <div>homehomehomehomehomehomehomehome</div>
+
+    </div>
+</script>
+
+{{--.page .login--}}
+<script type="text/ng-template" id="login.tpl" >
+    <div class="login container">
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+        <div>loginloginloginloginloginloginlogin</div>
+
+    </div>
+</script>
+
+{{--.page .signup--}}
+<script type="text/ng-template" id="signup.tpl" >
+    <div class="signup container">
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+        <div>signupsignupsignupsignupsignupsignup</div>
+
+    </div>
+</script>
+
 </html>
